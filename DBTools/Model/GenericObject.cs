afb2749df12a4a74cc49cc2285dc29ea9767af1d
Felipe Coelho R. Silva <reisfelipe18@hotmail.com>
2022-02-07 13:55:05 -0300

    Added entity implementation in Generic Object Class.

--- DBTools/Model/GenericObject.cs
+++ DBTools/Model/GenericObject.cs
@@ -11,9 +11,27 @@ namespace DBTools.Model
     {
         public String[] columns { get; set; }
         public Object[] values { get; set; }
-        public String [] valuesString { get; set; }
-     //  public DataView dataView { get; set; }
+        public String[] valuesString { get; set; }
+        //  public DataView dataView { get; set; }
         public String[] types { get; set; }
-        
+
+        public String table { get; set; }
+
+
+        public bool Insert(DBTools_Utilities.Utils dBTools)
+        {
+            return dBTools.Insert(columns, table, valuesString);
+        }
+
+        public bool Update(DBTools_Utilities.Utils dBTools, string conditions)
+        {
+            return dBTools.Update(columns, table, valuesString, conditions);
+        }
+
+        public bool Delete(DBTools_Utilities.Utils dBTools, string conditions)
+        {
+            return dBTools.Delete(table, conditions);
+        }
+
     }
 }
