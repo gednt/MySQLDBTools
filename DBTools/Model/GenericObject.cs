cdcb66269c3b99a393927f16e5625ed3fa76010a
gednt <big.gamesedicas@gmail.com>
2022-02-14 07:52:24 -0300

    Improvements in the GenericObject Model Class.

--- DBTools/Model/GenericObject.cs
+++ DBTools/Model/GenericObject.cs
@@ -1,37 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DBTools.Model
 {
     public class GenericObject
     {
+        /// <summary>
+        /// Initiates a new instance of a Generic Object, not able to do operations in the database.
+        /// </summary>
+        /// <param name="dbTools"></param>
+        public GenericObject()
+        {
+
+        }
+
+        /// <summary>
+        /// Initiates a new instance of a Generic Object, able to do operations in the database.
+        /// </summary>
+        /// <param name="dbTools"></param>
+        public GenericObject(DBTools_Utilities.Utils dbTools)
+        {
+            this.dbTools = dbTools;
+        }
+        private DBTools_Utilities.Utils dbTools = new DBTools_Utilities.Utils();
         public String[] columns { get; set; }
         public Object[] values { get; set; }
         public String[] valuesString { get; set; }
         //  public DataView dataView { get; set; }
         public String[] types { get; set; }
-
         public String table { get; set; }
 
 
-        public bool Insert(DBTools_Utilities.Utils dBTools)
+        public bool Insert()
         {
-            return dBTools.Insert(columns, table, valuesString);
+            return dbTools.Insert(columns, table, valuesString);
         }
 
-        public bool Update(DBTools_Utilities.Utils dBTools, string conditions)
+        public bool Update(string conditions)
         {
-            return dBTools.Update(columns, table, valuesString, conditions);
+            return dbTools.Update(columns, table, valuesString, conditions);
         }
 
-        public bool Delete(DBTools_Utilities.Utils dBTools, string conditions)
+        public bool Delete(string conditions)
         {
-            return dBTools.Delete(table, conditions);
+            return dbTools.Delete(table, conditions);
         }
-
     }
 }
