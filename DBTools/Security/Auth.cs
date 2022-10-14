330219e44d810619cae8f503d8f0dca8f72e2597
gednt <big.gamesedicas@gmail.com>
2022-10-14 13:37:18 -0300

    Code reorganization

---
+++ DBTools/Security/Auth.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+namespace DBTools.Security
+{
+    public class Auth
+    {
+        public String ConvertToSHA256(String value)
+        {
+            var crypt = new SHA256Managed();
+            var hash = new System.Text.StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(value));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+
+            return hash.ToString();
+        }
+
+        public String ConvertToSHA512(String value)
+        {
+            var crypt = new SHA512Managed();
+            var hash = new System.Text.StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(value));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+
+            return hash.ToString();
+        }
+
+      
+    }
+}
